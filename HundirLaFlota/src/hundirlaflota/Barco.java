fba0c7d35248936cdee920593430c741ff1a666f
lopmenal <alexetelopez@gmail.com>
2024-03-08 13:20:29 +0100

    Cambio4

--- HundirLaFlota/src/hundirlaflota/Barco.java
+++ HundirLaFlota/src/hundirlaflota/Barco.java
@@ -9,5 +9,32 @@ package hundirlaflota;
  * @author delcrego
  */
 public class Barco {
+    protected String nombre;
+    protected int longitud;
+    protected String figura;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setLongitud(int longitud) {
+        this.longitud = longitud;
+    }
+
+    public void setFigura(String figura) {
+        this.figura = figura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getLongitud() {
+        return longitud;
+    }
+
+    public String getFigura() {
+        return figura;
+    }
     
 }
