5293eae55e9c51c7769fdeeef658cdff2cf65cbe
lopmenal <alexetelopez@gmail.com>
2024-03-11 12:25:45 +0100

    Cambio4

--- HundirLaFlota/src/hundirlaflota/Jugador.java
+++ HundirLaFlota/src/hundirlaflota/Jugador.java
@@ -12,12 +12,12 @@ import java.util.ArrayList;
 
 public class Jugador {
     protected String nick;
-    protected char[][] tablero;
+    protected int[][] tablero;
     protected ArrayList<Barco> flota;
 
     public Jugador(String nick) {
         this.nick = nick;
-        this.tablero = new char[10][10];
+        this.tablero = new int[10][10];
         inicializarTablero();
         this.flota = new ArrayList<>();
     }
@@ -31,25 +31,19 @@ public class Jugador {
     }
 
     public void colocarBarco(Barco barco, int fila, int columna, boolean esVertical) {
-        // Implementa la lógica para colocar el barco en el tablero
-        // Verifica si la posición está ocupada, si el barco es demasiado grande, etc.
-        // Actualiza el tablero con la figura del barco en las posiciones correspondientes
+        
     }
 
     public void mostrarTablero() {
-        // Muestra el tablero por pantalla
-        // Puedes imprimirlo de la manera que prefieras
+        
     }
 
     public void disparar(Jugador oponente, int fila, int columna) {
-        // Implementa la lógica para disparar al oponente
-        // Actualiza el tablero del oponente según el resultado del disparo
-        // Si acierta, marca con 'O', si falla, marca con 'X'
+      
     }
 
     public boolean haPerdido() {
-        // Verifica si todos los barcos del jugador han sido hundidos
-        // Devuelve true si ha perdido, false en caso contrario
+        
         
     }
 
@@ -57,7 +51,7 @@ public class Jugador {
         this.nick = nick;
     }
 
-    public void setTablero(char[][] tablero) {
+    public void setTablero(int[][] tablero) {
         this.tablero = tablero;
     }
 
@@ -69,7 +63,7 @@ public class Jugador {
         return nick;
     }
 
-    public char[][] getTablero() {
+    public int[][] getTablero() {
         return tablero;
     }
 
