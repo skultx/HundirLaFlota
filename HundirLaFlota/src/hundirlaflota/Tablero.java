4175fdf45bad6138907b05edc8dd93f9735bab78
skultx <151526964+skultx@users.noreply.github.com>
2024-03-14 13:47:11 +0100

    tablero2

--- HundirLaFlota/src/hundirlaflota/Tablero.java
+++ HundirLaFlota/src/hundirlaflota/Tablero.java
@@ -43,12 +43,11 @@ public class Tablero {
     }
     
     //creamos el metodo para meter los barcos en posicion
-    public void colocarBarco(){
-        
-        Barco barco= new Barco("", 0);
+    public void colocarBarco() {
+
+        Barco barco = new Barco("", 0);
         barco.añadirBarcos();
-        
-        
+
         for (Barco b : barco.flota) {
             System.out.println("Coloca tu " + b.getNombre() + " de " + b.getLongitud());
             System.out.println("filas[1,10]: ");
@@ -58,15 +57,40 @@ public class Tablero {
 
             System.out.println("columnas[A,J]: ");
             String columna = sc.nextLine().toUpperCase();
+
+            //Validar los parametros
+            if (fila < 1 || fila > 10 || columna.length() != 1 || columna.charAt(0) < 'A' || columna.charAt(0) > 'J') {
+                System.out.println("Posición incorrecta. Inténtalo de nuevo.");
+                colocarBarco();
+            }
+
+            // Pedir pos del barco
+            System.out.println("H para horizontal V para vertical: ");
+            char orientacion = sc.next().toUpperCase().charAt(0);
             
-            if(fila>=1 || fila<=10 && columna.length()==1 && 
-                columna.charAt(0)>='A' || columna.charAt(0)<='J'){
-                matriz[fila-1][columna.charAt(0)-65]=b.getFigura();
-            }else{
-                System.out.println("Posicion erronea");
+            // Colocar el barco
+            switch (orientacion) {
+                case 'H':
+                    if (columna.charAt(0) + b.getLongitud() - 1 > 'J') {
+                        System.out.println("El barco no cabe");
+                        colocarBarco();
+                    }   for (int i = 0; i < b.getLongitud(); i++) {
+                        matriz[fila - 1][columna.charAt(0) - 'A' + i] = b.getFigura().substring(i, i + 1);
+                    }   break;
+                case 'V':
+                    if (fila + b.getLongitud() - 1 > 10) {
+                        System.out.println("El barco no cabe");
+                        colocarBarco();
+                    }   for (int i = 0; i < b.getLongitud(); i++) {
+                        matriz[fila - 1 + i][columna.charAt(0) - 'A'] = b.getFigura().substring(i, i + 1);
+                    }   break;
+                default:
+                    System.out.println("Error introduce H o V");
+                    colocarBarco();
+                    return;
             }
         }
-            
+
     }
     public static void main(String[] args) {
         Tablero t = new Tablero();
